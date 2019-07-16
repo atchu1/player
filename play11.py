a9cd87711d1d18d4314348a4f5527a26463c1476
atchu1 <atchayaraj18@gmail.com>
2019-07-16 18:32:58 +0530

    Update play11.py

--- play11.py
+++ play11.py
@@ -1,5 +1,5 @@
-day=input()
-if(day=="Saturday" or day=="Sunday"  or day=="saturday" or day=="sunday"):
+days=input()
+if(days=="Saturday" or days=="Sunday"  or days=="saturday" or days=="sunday"):
   print("yes")
 else:
   print("no")
